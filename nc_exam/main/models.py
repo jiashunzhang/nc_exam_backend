8ca06fed4752b9b3b1de76a232ee565270a2a4ae
Carzpurzkey <carzpurzkey@live.com>
2018-09-12 22:45:01 +0800

    多处改进

--- nc_exam/main/models.py
+++ nc_exam/main/models.py
@@ -48,6 +48,7 @@ class Members(models.Model):
     work_type_id = models.ForeignKey(WorkType, on_delete=models.CASCADE, db_column='work_type_id')
     idcard = models.CharField(primary_key=True, max_length=18)
     position_id = models.ForeignKey(Position, on_delete=models.CASCADE, db_column='position_id')
+    allow_red_packet = models.BooleanField()
 
     def __str__(self):
         return self.name
@@ -141,7 +142,7 @@ class TestQuestions(models.Model):
     score = models.FloatField()
     sn = models.IntegerField()
 
-    def __st__():
+    def __str__():
         return 'The question id is:' + str(test_question_id)
 
     class Meta:
@@ -155,8 +156,140 @@ class ExamQuestions(models.Model):
     score = models.FloatField()
     sn = models.IntegerField()
 
-    def __st__():
+    def __str__():
         return 'The question id is:' + str(exam_question_id)
 
     class Meta:
         db_table = 'ExamQuestions'
+
+class SignInLog(models.Model):
+    weixin_open_id = models.CharField(primary_key=True, max_length=50)
+    count = models.IntegerField()
+    date_time = models.DateTimeField()
+
+    def __str__():
+        return wexin_open_id + 'signin count: ' + str(count)
+
+    class Meta:
+        db_table = "SignInLog"
+
+class RedPacketType(models.Model):
+    pt_id = models.AutoField(primary_key=True)
+    pt_name = models.CharField(max_length=20)
+
+    def __str__():
+        return 'Red packet is: ' + pt_name
+    
+    class Meta:
+        db_table = 'RedPacketType'
+
+class TestRedPackets(models.Model):
+    rp_id = models.AutoField(primary_key=True)
+    weixin_open_id = models.CharField(max_length=50)
+    amount = models.FloatField()
+    date_time = models.DateTimeField()
+    test_paper_id = models.ForeignKey(TestPapers, on_delete=models.CASCADE, db_column='test_paper_id')
+    red_packet_type_id = models.ForeignKey(RedPacketType, on_delete=models.CASCADE, db_column='red_packet_type_id')
+    
+    def __str__():
+        return 'Packet is: ' + str(rp_id)
+
+    class Meta:
+        db_table = 'TestRedPackets'
+
+class AccumulatePoints(models.Model):
+    weixin_open_id = models.CharField(primary_key=True, max_length=50)
+    points = models.FloatField()
+
+    def __str__():
+        return 'Point is: ' + str(points)
+
+    class Meta:
+        db_table = 'AccumulatePoints'
+
+class AccumulatePointsType(models.Model):
+    type_id = models.AutoField(primary_key=True)
+    type_name = models.CharField(max_length=20)
+    points = models.FloatField()
+    max_score = models.FloatField()
+    min_score = models.FloatField()
+
+    def __str__():
+        return 'Point is: ' + str(points)
+
+    class Meta:
+        db_table = 'AccumulatePointsType'
+
+class AccumulatePointsLog(models.Model):
+    ap_id = models.AutoField(primary_key=True)
+    points = models.FloatField()
+    test_paper_id = models.ForeignKey(TestPapers, on_delete=models.CASCADE, db_column='test_paper_id')
+    date_time = models.DateTimeField()
+    ap_type_id = models.ForeignKey(AccumulatePointsType, on_delete=models.CASCADE, db_column='ap_type_id')
+    weixin_open_id = models.CharField(max_length=50)
+
+    def __str__():
+        return 'Point is: ' + str(points)
+
+    class Meta:
+        db_table = 'AccumulatePointsLog'
+
+class V_Members(models.Model):
+    name = models.CharField(max_length=20)
+    weixin_open_id = models.CharField(max_length=50, primary_key=True)
+    dep_id = models.IntegerField()
+    work_type_id = models.IntegerField()
+    position_d = models.IntegerField()
+    allow_red_packet = models.BooleanField()
+    three_new = models.BooleanField()
+    deleted = models.BooleanField()
+    dep_name = models.CharField(max_length=50)
+    type_name = models.CharField(max_length=20)
+    position_name = models.CharField(max_length=20)
+
+    def __str__():
+        return 'Member is: ' + name
+
+    class Meta:
+        db_table = 'v_members'
+
+class V_Wrongs(models.Model):
+    question_id = models.IntegerField(primary_key=True)
+    answers = models.CharField(max_length=255)
+    test_paper_id = models.IntegerField()
+    test_question_id = models.IntegerField()
+    weixin_open_id = models.CharField(max_length=50)
+    paper_name = models.CharField(max_length=200)
+    question_title = models.CharField(max_length=255)
+    question_type = models.IntegerField()
+    question_answer_texts = models.CharField(max_length=500)
+    question_right_answers = models.CharField(max_length=20)
+    wrong_count = models.IntegerField()
+    date_time = models.DateTimeField()
+
+    def __str__():
+        return 'Question is: ' + str(question_id)
+
+    class Meta:
+        db_table = 'v_wrongs'
+
+class WrongNote(models.Model):
+    note_id = models.AutoField(primary_key=True)
+    weixin_open_id = models.CharField(max_length=50)
+    question_id = models.ForeignKey(Questions, on_delete=models.CASCADE, db_column='question_id')
+    note = models.CharField(max_length=150)
+
+    class Meta:
+        db_table = 'WrongNote'
+
+class NoticeBoard(models.Model):
+    nb_id = models.AutoField(primary_key=True)
+    nb_type = models.IntegerField()
+    content = models.CharField(max_length=255)
+    color = models.CharField(max_length=20)
+    date_start = models.DateTimeField()
+    date_stop = models.DateTimeField()
+    date_time = models.DateTimeField()
+
+    class Meta:
+        db_table = 'NoticeBoard'
--- nc_exam/static/js/utils.js
+++ nc_exam/static/js/utils.js
@@ -9,3 +9,37 @@ function setInnerText(element, text) {
         element.innerText = text;
     }
 }
+function objectDeepCopy(src) {
+    let ret = undefined;
+    if(Array.isArray(src))
+        ret = [];
+    else if(src instanceof Object)
+        ret = {};
+    else
+        return src;
+
+    for(let p in src) {
+        let copy = src[p];
+        if(Array.isArray(copy) || copy instanceof Object)
+            ret[p] = arguments.callee(copy);
+        else
+            ret[p] = src[p];
+    }
+    return ret;
+}
+function datetimeFormat(d, fmt) {
+    var o = {
+        "M+": d.getMonth() + 1,
+        "d+": d.getDate(),
+        "h+": d.getHours(),
+        "m+": d.getMinutes(),
+        "s+": d.getSeconds(),
+        "q+": Math.floor((d.getMonth() + 3) / 3),
+        "S": d.getMilliseconds()
+    };
+    if(/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (d.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for(let k in o)
+        if(new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
